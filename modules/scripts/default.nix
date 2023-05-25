42b19ce41b5f4638b9ba85741125030d9cdfab31
stewart86 <siwei.wong@gmail.com>
2023-05-25 13:27:52 +0800

    update: cava style

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -2,7 +2,7 @@
 
 let
   cava-internal = pkgs.writeShellScriptBin "cava-internal" ''
-    cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+    cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/ /g;s/1/▁/g;s/2/▂/g;s/3/▃/g;s/4/▄/g;s/5/▅/g;s/6/▆/g;s/7/▇/g;'
   '';
   wallpaper_random = pkgs.writeShellScriptBin "wallpaper_random" ''
     killall dynamic_wallpaper
@@ -59,16 +59,6 @@ let
     }
 
     socat - UNIX-CONNECT:/tmp/hypr/$(echo $HYPRLAND_INSTANCE_SIGNATURE)/.socket2.sock | while read line; do border_color $line; done
-  '';
-
-  systemd_reset = pkgs.writeShellScriptBin "systemd_reset" ''
-    swww kill
-    systemctl stop home-manager-stewart.service
-    systemctl --user stop graphical-session.target
-    systemctl --user stop hyprland-session.target
-    systemctl --user daemon-reload
-    systemctl --user reset-failed
-    systemctl start home-manager-stewart.service
   '';
 in
 {
