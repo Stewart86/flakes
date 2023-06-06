b0892b3c34dfd17cb4466d744573592b310814d5
stewart86 <siwei.wong@gmail.com>
2023-06-06 20:47:35 +0800

    fish: add git abbrs

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -3,13 +3,18 @@
 {
   programs.fish = {
     enable = true;
-    shellAliases = {
-      tree = "lsd --tree";
-    };
+    shellAliases = { };
     shellAbbrs = {
       ls = "lsd -A";
       ll = "lsd -l -A";
       tree = "lsd --tree";
+
+      # Git
+      gc = "git add . && git commit -m ";
+      gca = "git add . && git commit -m 'auto commit' && git push";
+      gp = "git push";
+      gpl = "git pull";
+      lg = "lazygit";
     };
     interactiveShellInit = ''
       oh-my-posh init fish --config ~/.config/fish/omp_theme/stewart.omp.json | source
