d0b8d141df9b908f9c315556e20ad1fc8a75d6e0
stewart86 <siwei.wong@gmail.com>
2023-06-06 20:58:19 +0800

    fish: remove unused functions

--- modules/shell/fish/fish.nix
+++ modules/shell/fish/fish.nix
@@ -25,12 +25,8 @@
       oh-my-posh
     ];
   };
-  home.file.".config/fish/functions/fish_prompt.fish".source = ./functions/fish_prompt.fish;
-  home.file.".config/fish/functions/fishsauce.fish".text = import ./functions/fishsauce.nix;
   home.file.".config/fish/functions/n.fish".text = import ./functions/n.nix;
-  home.file.".config/fish/functions/r.fish".text = import ./functions/r.nix;
   home.file.".config/fish/functions/sd.fish".text = import ./functions/sd.nix;
-  home.file.".config/fish/functions/top.fish".text = import ./functions/top.nix;
   home.file.".config/fish/functions/vsd.fish".text = import ./functions/vsd.nix;
 
   home.file.".config/fish/fish_variables".text = import ./fish_variables.nix;
--- modules/shell/fish/functions/fishsauce.nix
+++
@@ -1,7 +0,0 @@
-''
-function fishsauce
-  if test -n "$VIRTUAL_ENV"
-    source "$VIRTUAL_ENV/bin/activate.fish"
-  end
-end
-''
