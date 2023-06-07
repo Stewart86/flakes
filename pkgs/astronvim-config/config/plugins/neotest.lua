2f1650b743a2e4ece6f47782c7adb8cee45c610f
stewart86 <siwei.wong@gmail.com>
2023-06-07 11:16:31 +0800

    nvim: revert neotest config

--- pkgs/astronvim-config/config/plugins/neotest.lua
+++ pkgs/astronvim-config/config/plugins/neotest.lua
@@ -19,13 +19,6 @@ return {
     }
   end,
   ft = { "rust", "python" },
-  keys = {
-    { "<leader>Tt", function() require("neotest").run.run() end,                  "Run test" },
-    { "<leader>Ts", function() require("neotest").summary.toggle() end,           "Toggle summary" },
-    { "<leader>To", function() require("neotest").output.open() end,              "Open output" },
-    { "<leader>TP", function() require("neotest").output_panel.toggle() end,      "Toggle output panel" },
-    { "<leader>Tf", function() require("neotest").run.run(vim.fn.expand "%") end, "Run current file" },
-  },
   dependencies = {
     "nvim-neotest/neotest-python",
     "rouge8/neotest-rust",
