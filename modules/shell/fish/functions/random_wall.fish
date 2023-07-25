8a9d742a95af8d55f66aeba856c62709d23d8817
stewart86 <siwei.wong@gmail.com>
2023-07-25 11:40:16 +0800

    fix: wallpaper on startup

--- modules/shell/fish/functions/random_wall.fish
+++ modules/shell/fish/functions/random_wall.fish
@@ -1,5 +1,5 @@
 function random_wall
-  if pgrep -f swww > /dev/null
+  if not pgrep -f swww > /dev/null
     swww init
   end
   swww img (fd . ~/Pictures/wallpapers/ -t file -e jpg -e png | shuf -n1)
