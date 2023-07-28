d5cf2ccc3832868978b0e3fd2c17448dae1c8652
stewart86 <siwei.wong@gmail.com>
2023-07-28 12:34:01 +0800

    try fix

--- pkgs/astronvim-config/config/plugins/treesitter.lua
+++ pkgs/astronvim-config/config/plugins/treesitter.lua
@@ -1,36 +1,33 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      -- {
+      --   "andymass/vim-matchup",
+      --   init = function() vim.g.matchup_matchparen_deferred = 1 end,
+      -- },
+      {
+        "HiPhish/nvim-ts-rainbow2",
+        config = function()
+          vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
+            callback = function()
+              vim.cmd.TSDisable "rainbow"
+              vim.cmd.TSEnable "rainbow"
+            end,
+          })
+        end,
+      },
+    },
+    opts = {
+      auto_install = true,
+      highlight = { disable = { "help" } },
+      -- matchup = { enable = true },
+      rainbow = { enable = true },
+    },
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   dependencies = {
-  --     {
-  --       "andymass/vim-matchup",
-  --       init = function() vim.g.matchup_matchparen_deferred = 1 end,
-  --     },
-  --     {
-  --       "HiPhish/nvim-ts-rainbow2",
-  --       config = function()
-  --         vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
-  --           callback = function()
-  --             vim.cmd.TSDisable "rainbow"
-  --             vim.cmd.TSEnable "rainbow"
-  --           end,
-  --         })
-  --       end,
-  --     },
-  --   },
-  --   opts = {
-  --     auto_install = true,
-  --     highlight = { disable = { "help" } },
-  --     matchup = { enable = true },
-  --     rainbow = { enable = true },
-  --   },
-  -- },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter-context",
-  --   dependencies = "nvim-treesitter/nvim-treesitter",
-  --   event = "BufEnter",
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = "BufEnter",
+  },
 }
