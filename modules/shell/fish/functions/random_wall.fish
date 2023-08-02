9a4b85b75f23e93a766d4410d2d4dcabfa22a533
stewart86 <siwei.wong@gmail.com>
2023-08-02 10:53:06 +0800

    try fix

--- modules/shell/fish/functions/random_wall.fish
+++ modules/shell/fish/functions/random_wall.fish
@@ -1,6 +1,6 @@
 function random_wall
   if not pgrep -f swww > /dev/null
-    swww init
+    swww init --no-cache
   end
   swww img (fd . ~/Pictures/wallpapers/ -t file -e jpg -e png | shuf -n1)
 end
