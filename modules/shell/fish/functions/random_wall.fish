5d47203c6f2dbdc4ac604a0b0817fefb85198bc0
stewart86 <siwei.wong@gmail.com>
2023-09-16 15:30:23 +0800

    update random_wall

--- modules/shell/fish/functions/random_wall.fish
+++ modules/shell/fish/functions/random_wall.fish
@@ -1,8 +1,6 @@
 function random_wall
-  sleep 8
   if not pgrep -f swww > /dev/null
     swww init
-    sleep 8
   end
   swww img (fd . ~/Pictures/wallpapers/ -t file -e jpg -e png | shuf -n1)
 end
