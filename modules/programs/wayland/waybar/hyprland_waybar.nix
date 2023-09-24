7afd6d858754e81ae380982483af2d69432123fc
stewart86 <siwei.wong@gmail.com>
2023-09-25 00:46:05 +0800

    update waybar config

--- modules/programs/wayland/waybar/hyprland_waybar.nix
+++ modules/programs/wayland/waybar/hyprland_waybar.nix
@@ -68,10 +68,7 @@
             "4" = "󰇍";
             "5" = "󰇎";
             "6" = "󰇏";
-            "active" = "";
-            "default" = "";
-            "urgent" = "";
-            "special" = "";
+            "special" = "󱝂";
           };
         };
         "backlight" = {
