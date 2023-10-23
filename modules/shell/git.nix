7d3dce781f41cd3ec79290b64844e36f24e2879e
stewart86 <siwei.wong@gmail.com>
2023-10-23 16:13:06 +0800

    add git gpg signing

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -4,6 +4,14 @@
       enable = true;
       userName = "stewart86";
       userEmail = "siwei.wong@gmail.com";
+      config = {
+        init = {
+          defaultBranch = "main";
+        };
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/github.pub";
+        commit.gpgsign = true;
+      };
     };
   };
 }
