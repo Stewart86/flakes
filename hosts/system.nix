035f1f57b47dbe1b9e5dc8e814020283d1631b14
stewart86 <siwei.wong@gmail.com>
2023-11-02 11:03:32 +0800

    add cachix

--- hosts/system.nix
+++ hosts/system.nix
@@ -50,9 +50,11 @@
 
   nix = {
     settings = {
-      # substituters = [
-      #   "https://cache.nixos.org/"
-      # ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
       auto-optimise-store = true;
     };
     gc = {
