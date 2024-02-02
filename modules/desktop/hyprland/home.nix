b3a77346fe7fd5dac3c6dda4ef75628773faa61d
stewart86 <siwei.wong@gmail.com>
2024-02-02 22:17:09 +0800

    try fix hyprland

--- modules/desktop/hyprland/home.nix
+++ modules/desktop/hyprland/home.nix
@@ -14,6 +14,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = true;
+    xwayland.enable = true;
     enableNvidiaPatches = false;
     extraConfig = builtins.readFile ./hyprland.conf;
   };
