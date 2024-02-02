ed2a4b310a5bb3a7c16c072ff60efdadb523f610
stewart86 <siwei.wong@gmail.com>
2024-02-02 23:07:34 +0800

    update config

--- modules/environment/hypr-variables.nix
+++ modules/environment/hypr-variables.nix
@@ -6,21 +6,19 @@
       DEFAULT_BROWSER = "brave";
       TERMINAL = "kitty";
       QT_QPA_PLATFORMTHEME = "gtk3";
-      QT_SCALE_FACTOR = "1";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      MOZ_ENABLE_WAYLAND = "1";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      SDL_VIDEODRIVER = "wayland";
+      # QT_SCALE_FACTOR = "1";
+      # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      # MOZ_ENABLE_WAYLAND = "1";
+      # QT_QPA_PLATFORM = "wayland";
+      # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      # SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
       _JAVA_AWT_WM_NONREPARENTING = "1";
-      WLR_DRM_DEVICES = "/dev/dri/card0";
+      # WLR_DRM_DEVICES = "/dev/dri/card0";
       XDG_SESSION_TYPE = "wayland";
       XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_DESKTOP = "Hyprland";
       XDG_BIN_HOME = "\${HOME}/.local/bin";
-      GDK_SCALE = "2";
-      XCURSOR_SIZE = "32";
     };
   };
 }
