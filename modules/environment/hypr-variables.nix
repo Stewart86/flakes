251665b6e9849de67bd0019a1d2509eefc61b725
stewart86 <siwei.wong@gmail.com>
2024-03-06 11:21:13 +0800

    change scaling on laptop screen
    change scaling on laptop screen
    
    change scaling on laptop screen
    
    change scaling on laptop screen
    
    change scaling on laptop screen

--- modules/environment/hypr-variables.nix
+++ modules/environment/hypr-variables.nix
@@ -11,6 +11,8 @@
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       GDK_BACKEND = "wayland,x11";
+      GDK_SCALE = "2";
+      XCURSOR_SIZE = "32";
       MOZ_ENABLE_WAYLAND = "1";
       SDL_VIDEODRIVER = "wayland";
       _JAVA_AWT_WM_NONREPARENTING = "1";
