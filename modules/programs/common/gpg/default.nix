2c1cfb623300e0cd8d03c2d32fbfa5420720748a
stewart86 <siwei.wong@gmail.com>
2024-03-30 23:44:05 +0800

    update flakes

--- modules/programs/common/gpg/default.nix
+++ modules/programs/common/gpg/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
+  programs.gpg.enable = true;
   programs.gpg.package = pkgs.gnupg;
   services = {
     gpg-agent = {
       enable = true;
-      pinentryFlavor = "gnome3";
+      pinentryPackage = pkgs.pinentry-gnome3;
     };
   };
 }
