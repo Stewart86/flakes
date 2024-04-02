99838c9699dcff0b5062b124b0953a53a041a711
stewart86 <siwei.wong@gmail.com>
2024-04-02 13:15:17 +0800

    configure fcitx

--- modules/environment/hypr-variables.nix
+++ modules/environment/hypr-variables.nix
@@ -11,6 +11,10 @@
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       QT_IM_MODULE = "fcitx";
+      GTK_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      XIM_SERVERS = "fcitx";
+      NIXOS_OZONE_WL = "1";
       GDK_BACKEND = "wayland,x11";
       GDK_SCALE = "1.2";
       XCURSOR_SIZE = "24";
