2eb1722bd000eaf743fb5dea56d57983e1aa005e
stewart86 <siwei.wong@gmail.com>
2024-04-06 21:11:03 +0800

    fixup! add cz config

--- modules/programs/common/lazygit/default.nix
+++ modules/programs/common/lazygit/default.nix
@@ -3,13 +3,14 @@
     packages = with pkgs; [
       lazygit
       pkgs.commitizen
+      pkgs.nodePackages."cz-emoji-conventional"
     ];
   };
   home.file.".config/lazygit/config.yml".text = ''
     customCommands:
       - key: "C"
         command: "git cz"
-        description: "commit with commitizen (cz)"
+        description: "commit with commitizen"
         context: "files"
         loadingText: "opening commitizen commit tool"
         subprocess: true
@@ -18,5 +19,10 @@
         context: "remotes"
         loadingText: "Pruning..."
         description: "prune deleted remote branches"
+  '';
+  home.file.".czrc".text = ''
+    {
+      "path": "cz-emoji-conventional"
+    }
   '';
 }
