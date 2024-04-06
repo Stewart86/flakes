7a3ab73c83103cac968fe64ce00ed0312b3b2981
stewart86 <siwei.wong@gmail.com>
2024-04-06 20:39:47 +0800

    add commitizen

--- modules/programs/common/lazygit/default.nix
+++ modules/programs/common/lazygit/default.nix
@@ -1,8 +1,22 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   home = {
     packages = with pkgs; [
       lazygit
+      cz-cli
     ];
   };
+  home.file.".config/lazygit/config.yml".text = ''
+    customCommands:
+      - key: "C"
+        command: "git cz"
+        description: "commit with commitizen"
+        context: "files"
+        loadingText: "opening commitizen commit tool"
+        subprocess: true
+      - key: "<c-p>"
+        command: "git remote prune {{.SelectedRemote.Name}}"
+        context: "remotes"
+        loadingText: "Pruning..."
+        description: "prune deleted remote branches"
+  '';
 }
