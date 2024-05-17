d889060e940cd23250db601d7192414ca89af910
stewart86 <siwei.wong@gmail.com>
2024-05-17 16:43:46 +0800

    update flakes

--- hosts/system.nix
+++ hosts/system.nix
@@ -188,7 +188,7 @@
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
-    package = pkgs.nixVersions.unstable;
+    package = pkgs.nixVersions.latest;
     registry.nixpkgs.flake = inputs.nixpkgs;
     extraOptions = ''
       experimental-features = nix-command flakes
