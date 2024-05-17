07382f2613ca65231f7ed9a348f6ad553f226012
stewart86 <siwei.wong@gmail.com>
2024-05-17 18:41:42 +0800

    🐛 fix(fish-shell): format fish files

--- modules/shell/fish/functions/vsd.fish
+++ modules/shell/fish/functions/vsd.fish
@@ -1,5 +1,5 @@
 function vsd
-    if count $argv > /dev/null
+    if count $argv >/dev/null
         cd $(fd . $HOME --max-depth 5 -t d --color=always --hidden -I -E=node_modules -E=.cache -E=__pycache__ -E=.git -E=virtualenvs -E=virtualenv | fzf --border --cycle --layout=reverse --preview-window=wrap --preview 'lsd --tree --color always --icon always --depth 3 {}' --ansi --height=30 -q $argv)
     else
         cd $(fd . $HOME --max-depth 5 -t d --color=always --hidden -I -E=node_modules -E=.cache -E=__pycache__ -E=.git -E=virtualenvs -E=virtualenv | fzf --border --cycle --layout=reverse --preview-window=wrap --preview 'lsd --tree --color always --icon always --depth 3 {}' --ansi --height=30)
